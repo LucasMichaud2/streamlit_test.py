7b465508490a22a85c158072e39b51f46b064e6e
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-03 00:55:44 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+csv_url = 'https://github.com/LucasMichaud2/streamlit_test.py/blob/main/final_csv.csv'
+df = pd.read_csv(csv_url)
+
 
 st.title('Hello World')
+
+st.dataframe(df)
