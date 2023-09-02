7aa54f59753c3e392efb82f09d721a1071e077db
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-03 01:14:18 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,6 @@ import pandas as pd
 csv_url = 'https://raw.githubusercontent.com/LucasMichaud2/streamlit_test.py/main/final_csv.csv'
 df = pd.read_csv(csv_url)
 
-
-
+st.title('My first Dashboard')
 
 st.dataframe(df)
