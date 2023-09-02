e731c2f7625f7ef8ffe100b7a57b108db3aefb69
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-03 01:31:37 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 csv_url = 'https://raw.githubusercontent.com/LucasMichaud2/streamlit_test.py/main/final_csv.csv'
 df = pd.read_csv(csv_url)
@@ -11,7 +12,7 @@ st.dataframe(df)
 df['Formats'] = df['channel'] + ' ' + df['formats']
 df = df.drop(columns=['channel'])
 df = df.drop(columns=['formats'])
-
+df['consideration'] = np.exp(df['consideration']
 st.dataframe(df)
 
 st.bar_chart(df, x='Formats', y='consideration')
