617ab062baded58655ae3f4c2a28829e4b728ca8
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-04 14:10:12 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 csv_url = 'https://raw.githubusercontent.com/LucasMichaud2/streamlit_test.py/main/final_csv.csv'
@@ -23,23 +22,6 @@ st.dataframe(df)
 st.bar_chart(df, x='Formats', y='consideration')
 
 
-# Sample data for the heatmap
-data = {
-    'A': [10, 20, 30],
-    'B': [40, 50, 60],
-    'C': [70, 80, 90]
-}
 
-# Create a DataFrame
-df_heatmap = pd.DataFrame(data)
-
-# Calculate the correlation matrix
-corr_matrix = df_heatmap.corr()
-
-# Create the heatmap using seaborn
-plt.figure(figsize=(6, 4))
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', center=0)
-
-# Display the heatmap using Streamlit
-st.title('Heatmap Example')
-st.pyplot()
+with st.sidebar:
+  container = st.container()
