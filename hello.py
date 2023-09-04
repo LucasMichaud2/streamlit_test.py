29a4d25e8a753b2d4dc87bc87134b0e5041815c3
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-04 14:29:24 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -27,3 +27,26 @@ with st.sidebar:
   st.write('this is a side bar')
   with st.container():
     st.write('this is a container')
+
+
+import streamlit as st
+
+st.title("Colored Box Example")
+
+# Define colors
+color1 = "#FF5733"
+color2 = "#33FF57"
+color3 = "#5733FF"
+
+# Create colored boxes using Markdown with CSS
+st.markdown(
+    f"""
+    <div style='display: flex;'>
+        <div style='background-color:{color1}; width: 100px; height: 100px; margin-right: 10px;'></div>
+        <div style='background-color:{color2}; width: 100px; height: 100px; margin-right: 10px;'></div>
+        <div style='background-color:{color3}; width: 100px; height: 100px;'></div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
