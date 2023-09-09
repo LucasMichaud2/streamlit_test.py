29bd50d9a64b48f9cc9a99a055bc37d20ca2357e
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-09 14:19:10 +0400

    Create gamned_class.py

---
+++ gamned_class.py
@@ -0,0 +1,126 @@
+class GAMNED_UAE:
+
+
+  def __init__(self, data, rating):
+    self.df_data = data[data['country'] == 'uae']
+    self.df_rating = rating
+    self.obj_list = ['branding', 'consideration', 'conversion']
+
+  def get_age_data(self):
+    column_names = ['channel', '13-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65+']
+    col1 = ['instagram', 'facebook', 'linkedin', 'snapchat', 'youtube']
+    col2 = [8, 4.7, 0, 20, 0]
+    col3 = [31, 21.5, 21.7, 38.8, 15]
+    col4 = [30, 34.3, 60, 22.8, 20.7]
+    col5 = [16, 19.3, 10, 13.8, 16.7]
+    col6 = [8, 11.6, 5.4, 3.8, 11.9]
+    col7 = [4, 7.2, 2.9, 0, 8.8]
+    col8 = [3, 5.6, 0, 0, 9]
+
+    df_age = pd.DataFrame(col1, columns = ['channel'])
+    df_age['13-17'] = col2
+    df_age['18-24'] = col3
+    df_age['25-34'] = col4
+    df_age['35-44'] = col5
+    df_age['45-54'] = col6
+    df_age['55-64'] = col7
+    df_age['65+'] = col8
+
+    return df_age
+
+
+
+  def uniform_channel(self):
+    # make sure we have one exhaustive channel list
+    channel_list = pd.concat([self.df_data['channel'], self.df_rating['channel']], axis=0)
+    channel_list = channel_list.unique().tolist()
+    return channel_list
+
+
+  def get_mean_rating(self):
+    # Get the rating df and output the mean value for each channel
+    # The df is all the channels and mean rating in relation to (branding,
+    # consideration and conversion)
+    mean_ratings = self.df_rating.drop('formats', axis=1)
+    mean_ratings = mean_ratings.groupby('channel').mean().reset_index()
+    return mean_ratings
+
+  def get_data_freq(self):
+    # get the freq of channel used in data for each objectives
+    df_freq = self.df_data[['objectif', 'channel']]
+    stack_list = []
+    for i in self.obj_list:
+      temp_df = df_freq[df_freq['objectif'] == i]
+      stack_list.append(temp_df['channel'].value_counts())
+    df_freq = pd.DataFrame(stack_list)
+    df_freq = df_freq.T
+    df_freq.columns = self.obj_list
+    df_freq = df_freq / df_freq.sum()
+    df_freq = df_freq*10
+    df_freq = df_freq.reset_index()
+    df_freq.rename(columns={'index': 'channel'}, inplace=True)
+    return df_freq
+
+  def get_channel_rating(self, input_age, df_age, df_freq, df_rating):
+    # Get the final rating of channel and formats depending on age group
+    # and objective
+
+    age_column = df_age[input_age].tolist()
+    age_channel = df_age['channel'].tolist()
+
+    age_dic = {
+        'channel': age_channel,
+        'branding': age_column,
+        'consideration': age_column,
+        'conversion': age_column
+    }
+    age_table = pd.DataFrame(age_dic)
+    age_table.iloc[0:, 1:] =  age_table.iloc[0:, 1:] / 10
+
+    temp1 = pd.concat([df_freq, age_table], axis=0)
+    temp1 = pd.concat([temp1, df_rating], axis=0)
+
+    df_channel_rating = temp1.groupby('channel').sum()
+    #df_channel_rating.columns = ['channel', 'branding', 'consideration', 'converison']
+    df_channel_rating = df_channel_rating.reset_index()
+
+    return df_channel_rating
+
+
+  def get_format_rating(self, channel_rating):
+    # combine format and channel rating
+
+    for index, row in channel_rating.iterrows():
+
+      a_value = row['channel']
+      self.df_rating.loc[self.df_rating['channel'] == a_value, self.obj_list] += row[self.obj_list]
+
+    return self.df_rating
+
+
+  def get_objective(self, input_obj, df_rating):
+
+    df_heatmap = df_rating[['channel', 'formats', input_obj]]
+    df_heatmap = df_heatmap.sort_values(by=input_obj, ascending=False)
+    return df_heatmap
+
+
+  def get_target(self):
+
+    target_dic = {
+        'channel': ['linkedin', 'search', 'video', 'native ads'],
+        'branding': [10, 10, 10, 10],
+        'consideration': [10, 10, 10, 10],
+        'conversion': [10, 10, 10, 10]
+    }
+
+    df_target = pd.DataFrame(target_dic)
+
+    return df_target
+
+  def add_target(self, df_target, channel_rating):
+
+      total_rating = pd.concat([channel_rating, df_target], axis=0)
+      total_rating = total_rating.groupby('channel').sum()
+
+      return total_rating
