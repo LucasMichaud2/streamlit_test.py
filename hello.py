fcdab363606e3aedee0bf9a96e0d14d1fd5c25d9
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-12 11:41:57 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -168,7 +168,7 @@ df_freq['conversion'] = df_freq['conversion'].round(1)
 
 
 
-st.area_chart(data=df_freq, x='channel', y=['branding', 'consideration', 'conversion'])
+st.bar_chart(data=df_freq, x='channel', y=['branding', 'consideration', 'conversion'])
 
 
 st.dataframe(df_freq)
