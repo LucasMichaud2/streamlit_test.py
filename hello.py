d2f15a0ee023dec67e96ec8ec05dea7045b4eb7f
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-20 12:36:06 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -466,8 +466,16 @@ with pie1:
   fig1, ax1 = plt.subplots()
   ax1.pie(df_brand['branding'], labels=df_brand['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors1)
   ax1.axis('equal')
-  ax1.set_title('Branding')
   st.pyplot(fig1)
+  st.text(' ')
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Branding</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
   
 
 with pie2:
