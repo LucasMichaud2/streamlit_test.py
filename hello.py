588b5fe0b5418df342a7a54117d521f227beb4e9
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-20 12:48:57 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -377,7 +377,67 @@ df_freq['conversion'] = df_freq['conversion'].round(1)
 
 
 
-st.bar_chart(data=df_freq, x='channel', y=['branding', 'consideration', 'conversion'])
+df_brand = df_freq[['channel', 'branding']]
+df_brand = df_brand.dropna()
+df_cons = df_freq[['channel', 'consideration']]
+df_cons = df_cons.dropna()
+df_conv = df_freq[['channel', 'conversion']]
+df_conv = df_conv.dropna()
+custom_colors1 = sns.color_palette('Blues', n_colors=len(df_brand))
+custom_colors2 = sns.color_palette('Purples', n_colors=len(df_cons))
+custom_colors3 = sns.color_palette('Reds', n_colors=len(df_conv))
+
+
+
+st.dataframe(df_brand)
+
+pie1, pie2, pie3 = st.columns(3)
+
+with pie1:
+
+  fig1, ax1 = plt.subplots()
+  ax1.pie(df_brand['branding'], labels=df_brand['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors1)
+  ax1.axis('equal')
+  st.pyplot(fig1)
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Branding</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+  
+
+with pie2:
+  
+  fig2, ax2 = plt.subplots()
+  ax2.pie(df_cons['consideration'], labels=df_cons['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors2)
+  ax2.axis('equal')
+  st.pyplot(fig2)
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Consideration</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+with pie3:
+  
+  fig3, ax3 = plt.subplots()
+  ax3.pie(df_conv['conversion'], labels=df_conv['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors3)
+  ax3.axis('equal')
+  st.pyplot(fig3)
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Conversion</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 
@@ -443,69 +503,9 @@ st.subheader('Budget Allocation')
 
 st.dataframe(df_allowance)
 
-st.dataframe(df_freq)
-
-df_brand = df_freq[['channel', 'branding']]
-df_brand = df_brand.dropna()
-df_cons = df_freq[['channel', 'consideration']]
-df_cons = df_cons.dropna()
-df_conv = df_freq[['channel', 'conversion']]
-df_conv = df_conv.dropna()
-custom_colors1 = sns.color_palette('Blues', n_colors=len(df_brand))
-custom_colors2 = sns.color_palette('Purples', n_colors=len(df_cons))
-custom_colors3 = sns.color_palette('Reds', n_colors=len(df_conv))
 
 
 
-st.dataframe(df_brand)
-
-pie1, pie2, pie3 = st.columns(3)
-
-with pie1:
-
-  fig1, ax1 = plt.subplots()
-  ax1.pie(df_brand['branding'], labels=df_brand['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors1)
-  ax1.axis('equal')
-  st.pyplot(fig1)
-  st.markdown(
-        """
-        <div style="display: flex; justify-content: center;">
-            <p style="text-align: center;">Branding</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-  
-
-with pie2:
-  
-  fig2, ax2 = plt.subplots()
-  ax2.pie(df_cons['consideration'], labels=df_cons['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors2)
-  ax2.axis('equal')
-  st.pyplot(fig2)
-  st.markdown(
-        """
-        <div style="display: flex; justify-content: center;">
-            <p style="text-align: center;">Consideration</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-with pie3:
-  
-  fig3, ax3 = plt.subplots()
-  ax3.pie(df_conv['conversion'], labels=df_conv['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors3)
-  ax3.axis('equal')
-  st.pyplot(fig3)
-  st.markdown(
-        """
-        <div style="display: flex; justify-content: center;">
-            <p style="text-align: center;">Conversion</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
 
 
 
