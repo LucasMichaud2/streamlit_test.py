964b11a677005e344603179141af151d4f843dcb
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-09-27 19:06:04 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -741,21 +741,26 @@ st.dataframe(df_allowance)
 
 st.text(' ')
 
-fig4, ax4 = plt.subplots()
-ax4.pie(df_allowance['allowance'], labels=df_allowance['channel'], startangle=90, wedgeprops=dict(width=0.4), colors=custom_colors1, autopct='%1.1f%%', pctdistance=0.85,
-       textprops=dict(color="black"))
+if input_budget == 0:
+  st.text('Awaiting for budget...')
 
+else: 
 
-center_circle = plt.Circle((0,0), 0.7, fc='white')
-fig4.gca().add_artist(center_circle)
-
-middle_text = ax4.text(0, 0, f"Total: {input_budget} USD", ha='center', va='center', fontsize=12, color='black', weight='bold')
-
-ax4.set_title('Budget Allocation')
-
-ax4.axis('equal')
-
-st.pyplot(fig4)
+  fig4, ax4 = plt.subplots()
+  ax4.pie(df_allowance['allowance'], labels=df_allowance['channel'], startangle=90, wedgeprops=dict(width=0.4), colors=custom_colors1, autopct='%1.1f%%', pctdistance=0.85,
+         textprops=dict(color="black"))
+  
+  
+  center_circle = plt.Circle((0,0), 0.7, fc='white')
+  fig4.gca().add_artist(center_circle)
+  
+  middle_text = ax4.text(0, 0, f"Total: {input_budget} USD", ha='center', va='center', fontsize=12, color='black', weight='bold')
+  
+  ax4.set_title('Budget Allocation')
+  
+  ax4.axis('equal')
+  
+  st.pyplot(fig4)
 
 
 
