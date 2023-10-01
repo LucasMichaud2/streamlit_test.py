e77c73da4c4819106caecd5c300a457403e46fc9
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-01 20:10:43 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -795,7 +795,7 @@ heatmap_data = heatmap_data.reset_index()
 st.dataframe(heatmap_data)
 data_matrix = heatmap_data["norm"].values.reshape(heatmap_size, heatmap_size)
 st.title('Second Heatmap')
-plt.figure(figsize=(20, 20))
+plt.figure(figsize=(25, 25))
 sns.heatmap(data_matrix, cmap="magma", annot=False, xticklabels=False, yticklabels=False, cbar=False)
 for i in range(heatmap_size):
   for j in range(heatmap_size):
