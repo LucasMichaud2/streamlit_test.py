bd5f04780c5a64611741540958f4f02c6ffa9cf4
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-01 20:24:14 +0400

    Update archive.py

--- archive.py
+++ archive.py
@@ -1,1 +1,223 @@
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color0}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name0}</div>
+       <div style='background-color:{color1}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name1}</div>
+       <div style='background-color:{color2}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name2}</div>
+       <div style='background-color:{color3}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name3}</div>
+       <div style='background-color:{color4}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name4}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
 
+
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color5}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name5}</div>
+      <div style='background-color:{color6}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name6}</div>
+      <div style='background-color:{color7}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name7}</div>
+      <div style='background-color:{color8}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name8}</div>
+      <div style='background-color:{color9}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name9}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color10}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name10}</div>
+      <div style='background-color:{color11}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name11}</div>
+      <div style='background-color:{color12}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name12}</div>
+      <div style='background-color:{color13}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name13}</div>
+      <div style='background-color:{color14}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name14}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color15}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name15}</div>
+      <div style='background-color:{color16}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name16}</div>
+      <div style='background-color:{color17}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name17}</div>
+      <div style='background-color:{color18}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name18}</div>
+      <div style='background-color:{color19}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name19}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color20}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name20}</div>
+      <div style='background-color:{color21}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name21}</div>
+      <div style='background-color:{color22}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name22}</div>
+      <div style='background-color:{color23}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name23}</div>
+      <div style='background-color:{color24}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name24}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color25}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name25}</div>
+      <div style='background-color:{color26}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name26}</div>
+      <div style='background-color:{color27}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name27}</div>
+      <div style='background-color:{color28}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name28}</div>
+      <div style='background-color:{color29}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name29}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color30}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name30}</div>
+      <div style='background-color:{color31}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name31}</div>
+      <div style='background-color:{color32}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name32}</div>
+      <div style='background-color:{color33}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name33}</div>
+      <div style='background-color:{color34}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name34}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+with st.container():
+  st.markdown(
+  f"""
+  <div style='display: flex;'>
+      <div style='background-color:{color35}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name35}</div>
+      <div style='background-color:{color36}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name36}</div>
+      <div style='background-color:{color37}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name37}</div>
+      <div style='background-color:{color38}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name38}</div>
+      <div style='background-color:{color39}; width: {'140px'}; height: {'75px'}; margin-riht: {'50px'}; font-size:{'10px'};
+      display: flex; align-items: {'center'}; justify-content: {'center'}; border-radius: {'20px'}; color: white;'>{name39}</div>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
+
+
+df_brand = df_freq[['channel', 'branding']]
+df_brand = df_brand.dropna()
+df_cons = df_freq[['channel', 'consideration']]
+df_cons = df_cons.dropna()
+df_conv = df_freq[['channel', 'conversion']]
+df_conv = df_conv.dropna()
+custom_colors1 = sns.color_palette('Blues', n_colors=len(df_brand))
+custom_colors2 = sns.color_palette('Purples', n_colors=len(df_cons))
+custom_colors3 = sns.color_palette('Reds', n_colors=len(df_conv))
+
+
+
+pie1, pie2, pie3 = st.columns(3)
+
+with pie1:
+
+  fig1, ax1 = plt.subplots()
+  ax1.pie(df_brand['branding'], labels=df_brand['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors1)
+  ax1.axis('equal')
+  st.pyplot(fig1)
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Branding</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+  
+
+with pie2:
+  
+  fig2, ax2 = plt.subplots()
+  ax2.pie(df_cons['consideration'], labels=df_cons['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors2)
+  ax2.axis('equal')
+  st.pyplot(fig2)
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Consideration</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+with pie3:
+  
+  fig3, ax3 = plt.subplots()
+  ax3.pie(df_conv['conversion'], labels=df_conv['channel'], autopct='%1.1f%%', startangle=90, colors=custom_colors3)
+  ax3.axis('equal')
+  st.pyplot(fig3)
+  st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <p style="text-align: center;">Conversion</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
