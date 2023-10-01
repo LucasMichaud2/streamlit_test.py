8d40704a71a07f750a6b880770be964f3bf0b5ad
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-01 20:03:21 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -264,8 +264,7 @@ df_rating3 = gamned_class.get_objective(selected_objective, df_rating2)
 full_format_rating = df_rating3.copy()
 format_rating = df_rating3.copy()
 format_rating['format'] = format_rating['channel'] + ' - ' + format_rating['formats']
-format_rating = format_rating.drop(['formats'], axis=1)
-format_rating = format_rating[['channel', 'format', selected_objective]]
+format_rating = format_rating[['channel', 'formts', 'format', selected_objective]]
 min_format = full_format_rating[selected_objective].min()
 max_format = full_format_rating[selected_objective].max()
 format_rating['norm'] = (format_rating[selected_objective] - min_format) / (max_format - min_format)*100
