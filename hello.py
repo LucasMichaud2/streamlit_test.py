36cf13973001a54e72145bada9ebfcd0c0f7d458
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-01 20:12:52 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -801,7 +801,7 @@ for i in range(heatmap_size):
   for j in range(heatmap_size):
     label = format_rating.at[i+j, 'channel'] + '\n' + '\n' + format_rating.at[i+j, 'formats']
     font_prop = fm.FontProperties(weight='bold', size=17)
-    plt.text(j + 0.5, i + 0.5, label, ha='center', color='white', fontsize=17, fontproperties=font_prop)
+    plt.text(j + 0.5, i + 0.5, label, ha='center', color='black', fontsize=17, fontproperties=font_prop)
     
 st.pyplot(plt)
 
